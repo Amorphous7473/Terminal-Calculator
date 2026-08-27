675354a5d55244b921d65a535a190511d968d66d
Amorphous <shrey7473@protonmail.com>
2026-08-28 00:59:01 +0530

    Finally done with the calculator and scrapped the mixed function

--- calculator.cpp
+++ calculator.cpp
@@ -1,11 +1,12 @@
 #include <cctype>
 #include <iostream>
+#include <ratio>
 #include<vector>
 #include <limits>
 #include<string>
 using namespace std;
 //All Arithmetic Classes
-int addition(vector<int> nums)
+float addition(vector<float> nums)
 {
     float ans = 0;
     for(float i: nums)
@@ -14,116 +15,43 @@ int addition(vector<int> nums)
     }
     return ans;
 }
-int subtraction(vector<int> nums)
+float subtraction(vector<float> nums)
 {
-    int ans = nums[0];
+    float ans = nums[0];
     for(int i = 1; i <nums.size(); i++)
     {
         ans -= nums[i];
     }
     return ans;
 }
-int multiplication(vector<int> nums)
+float multiplication(vector<float> nums)
 {
-    int ans = 1;
+    float ans = 1;
     for(int i: nums)
     {
         ans *= i;
     }
     return ans;
 }
-int division(vector<int> nums)
+float division(vector<float> nums)
 {
-    int ans = 1;
+    float ans = 1;
     for(int i: nums)
     {
         ans /= i;
     }
     return ans;
 }
-int modulo(vector<int> nums)
-{
-    int ans = 0;
-    for(int i: nums)
-    {
-        ans %= i;
-    }
-    return ans;
-}
-int mixed() {
-    string n;
-    int number, answer = 1, count = 0, arithmetic = 0;
-    cout << "In this mode, the equations will be calculated from the left to the right. It will not follow BODMAS Rule\n";
-    cin >> n;
-    for (char i : n) {
-        if (isdigit(i)) {
-            number = i - '0'; // Convert char to int
-            arithmetic = 0;
-            continue;
-        }
-        else if (i == '+') {
-            answer += number;
-            count++;
-            arithmetic++;
-            if (arithmetic > 1) {
-                cout << "Found an extra arithmetic " << "'" << "+" << "'" << ". Removing " << "+";
-            }
-            continue;
-        }
-        else if (i == '-') {
-            answer -= number;
-            count++;
-            arithmetic++;
-            if (arithmetic > 1) {
-                cout << "Found an extra arithmetic " << "'" << "-" << "'" << ". Removing " << "-";
-            }
-            continue;
-        }
-        else if (i == '*') {
-            answer *= number;
-            arithmetic++;
-            if (arithmetic > 1) {
-                cout << "Found an extra arithmetic " << "'" << "*" << "'" << ". Removing " << "*";
-            }
-            continue;
-        }
-        else if (i == '/') {
-            if (number == 0) {
-                cout << "Error: Division by zero is not allowed.\n";
-                return numeric_limits<int>::max(); // Return a large number to indicate error
-            }
-            answer /= number;
-            arithmetic++;
-            if (arithmetic > 1) {
-                cout << "Found an extra arithmetic " << "'" << "/" << "'" << ". Removing " << "/";
-            }
-            continue;
-        }
-        else if (i == '%') {
-            answer %= number;
-            arithmetic++;
-            if (arithmetic > 1) {
-                cout << "Found an extra arithmetic " << "'" << "%" << "'" << ". Removing " << "%";
-            }
-            continue;
-        }
-    }
-    if (count >= 1) {
-        answer--;
-    }
-    return answer;
-}
-
 //The main function
 int main()
 {
     //Variables
-    int a, i = 0;
+    int a;
     string n;
-    vector<int> nums;
+    vector<float> nums;
     //Cout Statements
     cout<<"Calculator:\n\nSelect the Type of Arithmetic operations by typing it's serial number";
-    cout<<"\n1.Addition\n2.Subtraction\n3.Multiplication\n4.Division\n5.Modulo\n6.Mixed\n";
+    cout<<"\n1.Addition\n2.Subtraction\n3.Multiplication\n4.Division\n";
     while(true)
     {
         cin>>a;
@@ -134,15 +62,10 @@ int main()
         cout<<"Please enter a number: ";
         continue;
         }
-        else if(a>6||a<1)
+        else if(a>5||a<1)
         {
-            cout<<"Please enter a number between 1 and 6: ";
+            cout<<"Please enter a number between 1 and 5: ";
             continue;
-        }
-        else if(a == 6)
-        {
-            cout<<mixed();
-            return 0;
         }
             break;
     }
@@ -167,7 +90,7 @@ int main()
         }
         }
         }
-        int blah;
+        float blah;
         if(!digit)
         {
             cout<<"Please enter a valid number";
